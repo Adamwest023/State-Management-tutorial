e86908cdb95dbe84c90b37b28c21779562e919ed
Adam West <Adamwest023@gmail.com>
2022-08-16 14:46:56 -0500

    created state logic for left and right states

--- JS/player.js
+++ JS/player.js
@@ -1,14 +1,16 @@
+import { StandingLeft, StandingRight } from "./state.js";
+
 export default class Player {
     constructor(gameWidth, gameHeight) {
         this.gameWidth = gameWidth;
         this.gameHeight = gameHeight;
-        this.states = [];
+        this.states = [new StandingLeft(), new StandingRight()];
         this.currentState = this.states[0];
         this.image = dogImage;
         this.width = 200;
         this.height = 181.83;
         this.x = this.gameWidth * .5 - this.width * .5;
-        this.y = this.gameHeight  - this.height;
+        this.y = this.gameHeight - this.height;
         this.frameX = 0;
         this.frameY = 0;
     }
@@ -16,6 +18,12 @@ export default class Player {
         //can expect 3,5,9 arguments
         context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
     }
-    update() {
+    update(input) {
+        this.currentState.handleInput(input);
     }
+    setState(state) {
+        this.currentState = this.states[state];
+        this.currentState.enter();
+    }
+
 }
---
+++ JS/state.js
@@ -0,0 +1,41 @@
+//use enum for states
+export const states = {
+    STANDING_LEFT: 0,
+    STANDING_RIGHT: 1,
+
+}
+
+class State {
+    constructor(state) {
+        this.state = state;
+
+    }
+}
+//child class of State
+export class StandingLeft extends State {
+    constructor(player) {
+        super('STANDING LEFT');
+        this.player = player;
+    }
+    enter() {
+        this.player.frameY = 1;
+
+    }
+    handleInput(input) {
+        if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT); //set state to StandingRight
+    }
+};
+//child class of State
+export class StandingRight extends State {
+    constructor(player) {
+        super('STANDING RIGHT');
+        this.player = player;
+    }
+    enter() {
+        this.player.frameY = 0;
+
+    }
+    handleInput(input) {
+        if (input === 'PRESS right') this.player.setState(states.STANDING_LEFT) //set state to StandingLeft
+    }
+};
