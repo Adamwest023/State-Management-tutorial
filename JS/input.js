891b2811256dc2e7a73927d3b5d0debd0ebec435
Adam West <Adamwest023@gmail.com>
2022-08-16 15:21:19 -0500

    added sitting states

--- JS/input.js
+++ JS/input.js
@@ -2,13 +2,19 @@ export default class InputHandler {
     constructor() {
         this.lastKey = '';
         //listens for a key being pressed
-        window.addEventListener('keydown', (e) =>  {
+        window.addEventListener('keydown', (e) => {
             switch (e.key) {
                 case "ArrowLeft":
                     this.lastKey = 'PRESS left';
                     break;
                 case "ArrowRight":
                     this.lastKey = 'PRESS right';
+                    break;
+                case "ArrowDown":
+                    this.lastKey = 'PRESS down';
+                    break;
+                case "ArrowUp":
+                    this.lastKey = 'PRESS up';
                     break;
             }
         });
@@ -20,6 +26,12 @@ export default class InputHandler {
                     break;
                 case "ArrowRight":
                     this.lastKey = 'RELEASE right';
+                    break;
+                case "ArrowDown":
+                    this.lastKey = 'RELEASE down';
+                    break;
+                case "ArrowUp":
+                    this.lastKey = 'RELEASE up';
                     break;
             }
         });
