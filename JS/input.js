788a1016b19e29a4ad06e27cf88ac85c4397535d
Adam West <Adamwest023@gmail.com>
2022-08-16 12:57:33 -0500

    added switch case to keystrokes

--- JS/input.js
+++ JS/input.js
@@ -1,6 +1,27 @@
 export default class InputHandler {
     constructor() {
         this.lastKey = '';
-        
+        //listens for a key being pressed
+        window.addEventListener('keydown', (e) =>  {
+            switch (e.key) {
+                case "ArrowLeft":
+                    this.lastKey = 'PRESS left';
+                    break;
+                case "ArrowRight":
+                    this.lastKey = 'PRESS right';
+                    break;
+            }
+        });
+        //listens for a key being released
+        window.addEventListener('keyup', (e) => {
+            switch (e.key) {
+                case "ArrowLeft":
+                    this.lastKey = 'RELEASE left';
+                    break;
+                case "ArrowRight":
+                    this.lastKey = 'RELEASE right';
+                    break;
+            }
+        });
     }
 }
